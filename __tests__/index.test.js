83a65bf9627aafcad13a9d0948a203e8aa7d0a33
unknown <vladimirzuev14@icloud.com>
2023-03-23 10:44:02 +0300

    fix test2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,29 @@
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
-import result1 from '../__fixtures__/resultStylish.js';
+import resultStylish from '../__fixtures__/resultStylish';
+import resultPlain from '../__fixtures__/resultPlain';
 
+const resultJson = JSON.stringify(JSON.parse(fs.readFileSync('__fixtures__/resultJson.json', 'utf8')));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const genDiff = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
-const cases = [
-  // ['file1.json', 'file2.json', 'resultJson.txt', 'json'],
-  ['file1.yml', 'file2.yml', 'resultStylish.js', 'stylish'],
-  ['file1.json', 'file2.json', 'resultStylish.js', 'stylish'],
-  // ['file1.json', 'file2.json', 'resultPlain.txt', 'plain'],
-];
-
-test.each(cases)('Compare %s and %s to expect %s in "%s" style', (firstArg, secondArg, expectedResult, format) => {
-  const firstFile = getFixturePath(firstArg);
-  const secondFile = getFixturePath(secondArg);
-  // const getResult = readFile(expectedResult);
-  const result = genDiff(firstFile, secondFile, format);
-  expect(result1).toEqual(result);
+test('test1 - fileJsonStylish', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(resultStylish);
+});
+test('test2 - fileYmlStylish', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(resultStylish);
+});
+test('test3 - fileYmlPlain', () => {
+  expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(resultPlain);
+});
+test('test4 - fileJsonStylish', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(resultPlain);
+});
+test('test5 - fileJson', () => {
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(resultJson);
 });
